a1748b5c10854707bc1ca2b0c34038ff4deed8bb
SVBrazhko <braninparol>
2022-01-03 21:53:53 +0200

    1

--- src/test/java/AlgorithmTest.java
+++ src/test/java/AlgorithmTest.java
@@ -13,73 +13,83 @@ public class AlgorithmTest {
     public static void main(String[] args) {
 
         Deque <Bet> queue = new ConcurrentLinkedDeque();
-        int maxLooseBetCount = 0;
+
+        int maxLooseBetCount = 6;
+        int totalBEt = 246;
+
         double total = 10000.0;
-        double credit = 0;
-        double minBet = 10.0;
-        double margin = 100.;
-        double maxMargin = 50;
+        double minBet = 1.0;
+        double margin = 1;
+
+
 
         int numberOfMatches = 0;
         Random random = new Random();
         BetService betService = new BetService();
         boolean isCreditBet = false;
-        while (numberOfMatches<100) {
+        while (numberOfMatches<100 & total > 0) {
+            if(total > 0 ) {
             numberOfMatches++;
             Match match = new Match("Match " +numberOfMatches,"Match " +numberOfMatches);
-            System.out.println(match.matchName + " ===========================");
+            System.out.println(match.matchName + " =========================" + queue.size());
             int countLooseBet = 0;
             for (Integer q:match.scoreMap.keySet()) {
-                if(total > 0 ) {
-
-                    if(queue.size()>0){
+                    if(queue.size()>0 & q==1) {
                         Bet prevBet = queue.pollLast();
                         if(prevBet !=null) {
                            countLooseBet = prevBet.numberOfLoosedBets;
                         }
                     }
 
-                    System.out.println("Quoter " + q+ " -------------------");
-                    if (q==1 & credit >20) {
-                        margin = maxMargin;
-                        isCreditBet = true;
-                    }
-                    if(isCreditBet) {
-                        credit -= maxMargin;
-                    }
                     OddEven nextOddEven = betService.getNextOddEven(match, q);
                     double nextBetValue = betService.getNextBetValue(match, q, minBet, margin, countLooseBet);
                     total -= nextBetValue;
 
                     Bet bet = new Bet(match,nextOddEven, nextBetValue,q);
-                    System.out.println("Bet " + nextBetValue + " - " + nextOddEven);
+
                     int score = random.nextInt(200);
                     match.scoreMap.put(q,score);
-                    System.out.println("Score " + score);
+                    String result ="";
                     if(bet.isWin()) {
                         double benefit = bet.value*1.9;
                         total+=benefit;
                         countLooseBet=0;
-                        System.out.println("WIN ");
+                        result = "WIN ";
                     } else {
                         countLooseBet++;
-                        System.out.println("LOOSE");
+                        result = "LOOSE";
                         bet.numberOfLoosedBets = countLooseBet;
-                        if(q==4){
-                            queue.addFirst(bet);
-                            credit +=bet.value;
+                        if(q==4) {
+                            if(bet.numberOfLoosedBets>maxLooseBetCount){
+                                for(int i =0;i<10;i++){
+                                    Double nextBetValue1 = betService.getNextBetValue(match, q, minBet, margin, bet.numberOfLoosedBets-2);
+                                    Bet bet1 = new Bet(match,nextOddEven, nextBetValue1,q);
+                                    bet1.numberOfLoosedBets = bet.numberOfLoosedBets-3;
+                                    queue.addFirst(bet1);
+                                }
+                            } else {
+                                queue.addFirst(bet);
+                            }
                         }
                     }
+                    System.out.println("Quoter " + q+ ", Bet " + + nextBetValue + " - " + nextOddEven +"("+bet.numberOfLoosedBets+")"+ ", Score " + score + ", RES "+ result);
+
                     System.out.println("Total " + total);
-                    System.out.println("Credit " + credit);
                     isCreditBet = false;
-                }
+
             }
             System.out.println("============================================");
             System.out.println("============================================");
+
+            }
         }
         System.out.println("Total - " + total);
-        System.out.println("Credit - " + credit);
+
+        double totalQueue = 0.0;
+        for (Bet b :queue){
+            totalQueue += b.value;
+        }
+        System.out.println("Total queue- " + queue.size() + ", " + totalQueue);
 
 
 
